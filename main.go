d8920c21a2bfac7ff2b555ec11c0c66986415f62
zhang-gap <1611291677@qq.com>
2024-12-12 15:40:47 +0800

    Update main.go

--- main.go
+++ main.go
@@ -4,10 +4,10 @@ package main
 // Import os so we can read environment variables from the system
 import (
 	"fmt"
-	"os"
+	//"os"
 )
 
 func main() {
 	fmt.Println("Hello, Kubernetes！I'm from Jenkins CI！")
-	fmt.Println("BRANCH_NAME:", os.Getenv("branch"))
+	//fmt.Println("BRANCH_NAME:", os.Getenv("branch"))
 }
